f413a97d3368035616767ae1dd4efd7083159cb8
mgirma <51236729+mgirma@users.noreply.github.com>
2021-01-20 18:23:15 -0500

    worked on jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,20 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+function Question(props) {
+  console.log(props);
+  return <div className="Question">{props.text}</div>;
+}
+
 function App() {
-  return <div className="app">Trivia!</div>;
+  var currentQuestion = 0;
+
+  return (
+    <div className="app">
+      Trivia!
+      <Question text={data[currentQuestion].question.text} />
+    </div>
+  );
 }
 
 export default App;
